ee25f69037be24c05a1937327d0f2a892b82ffa3
s1mba93 <danielelson93@gmail.com>
2025-04-06 06:13:35 +0100

    Asteroid Creation

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    AsteroidField.containers = (updateable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    Player.containers = (updateable, drawable)
+    
+    asteroidField = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
     
     # Main Game Loop
@@ -17,9 +29,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        updateable.update(dt)
+        
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
         
         
