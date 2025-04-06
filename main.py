fc4620cb113ea22136317497e56712cf0e9a25ee
s1mba93 <danielelson93@gmail.com>
2025-04-06 05:03:56 +0100

    add player creation

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     
+    # Main Game Loop
     while True:
+        # Check for the close window event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black")       
+        
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        
+        
+        
+        # Limit framerate
+        dt = clock.tick(60) / 1000
+        
     
     
     
